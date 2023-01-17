1b0a2f593c4506e862f7fdba5fbd28f87e28d2ed
med-redha <redhaboutahir@gmail.com>
2023-01-17 03:31:46 +0100

    feat: layout

--- src/components/snapChallenge.vue
+++ src/components/snapChallenge.vue
@@ -1,6 +1,15 @@
 <template>
- <div>
-  hellooooooooooooooooooooooooooooooo
+ <div class="landingPage">
+<v-row>
+  <v-col cols="6">
+    <div></div>
+  </v-col>
+  <v-col cols="6">
+    <div class="hero_section">
+      <img  class="hero_img" src="../assets/images/image-hero-desktop.png" alt="">
+    </div>
+  </v-col>
+</v-row>  
  </div>
 </template>
 
