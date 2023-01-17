14efd2c176d38bd57024bb481554d0041c30226e
med-redha <redhaboutahir@gmail.com>
2023-01-17 03:23:45 +0100

    feat:an emty structure to start the projects

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <snap-challenge/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SnapChallenge from './components/snapChallenge.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
+  components: {
+    SnapChallenge,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/main.scss';
 </style>
---
+++ src/components/snapChallenge.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+  hellooooooooooooooooooooooooooooooo
+ </div>
+</template>
+
+<script>
+  export default {
+    name: 'snapChallenge',
+
+    data(){
+      return{
+
+      };
+    },
+  }
+</script>
